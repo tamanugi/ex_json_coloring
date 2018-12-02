67b51ea3716110070aef7265dd889e6b62e8b6c7
tamanugi <tamanugi@gmail.com>
2018-12-02 16:51:03 +0900

    implement

--- lib/ex_json_coloring.ex
+++ lib/ex_json_coloring.ex
@@ -4,6 +4,7 @@ defmodule ExJsonColoring do
   """
 
   alias ExJsonColoring.Token
+  alias ExJsonColoring.Lexir
 
   @tokens [
     :square_bracket,
@@ -21,97 +22,26 @@ defmodule ExJsonColoring do
   
   ## Examples
 
-    iex>ExJsonColoring.coloring  "test"
-    "hogehoge"
+    iex>ExJsonColoring.coloring  ~s({"key": "value"})
+    "test"
 
   """
-  def coloring(str) do
-    "hogehoge"
+  def coloring(json) do
+    Lexir.lexir(json)
+    |> Enum.map(fn token ->
+      %Token{token: type, value: value} = token
+      color = case type do
+        :brace -> :color202
+        :key -> :color101
+        :colon -> :darkblue
+        :string -> :hotpink
+      end
+
+      [color, value]
+    end)
+    |> List.flatten
+    |> Bunt.ANSI.format
+    |> IO.puts
   end
-
-  @doc """
-  ## Examples
-
-    iex>ExJsonColoring.lexir ~s({"key": "value"})
-    [
-      %ExJsonColoring.Token{token: :brace, value: "{"},
-      %ExJsonColoring.Token{token: :key, value: "\\"key\\""},
-      %ExJsonColoring.Token{token: :colon, value: ":"},
-      %ExJsonColoring.Token{token: :string, value: "\\"value\\""},
-      %ExJsonColoring.Token{token: :brace, value: "}"}
-    ]
-  """
-  def lexir(json) do
-    json
-    |> parse(nil, [])
-  end
-
-  def parse("{" <> rest, state, tokens) do
-    token = %Token{token: :brace, value: "{"}
-    parse(rest, :find_key, [token] ++ tokens)
-  end
-
-  def parse("\"" <> rest, state, tokens) do
-    {string, rest} = string_continue(rest)
-
-    token_type = case state do
-      :find_key -> :key
-      _ -> :string
-    end
-
-    token = %Token{token: token_type, value: "\"#{string}\""}
-    parse(rest, :string, [token] ++ tokens)
-  end
-
-  def parse(":" <> rest, state, tokens) do
-    token = %Token{token: :colon, value: ":"}
-    parse(rest, state, [token] ++ tokens)
-  end
-
-  def parse("}" <> _, _, tokens) do
-    token = %Token{token: :brace, value: "}"}
-    [token] ++ tokens
-    |> Enum.reverse
-  end
-
-  def parse(" " <> rest, state, tokens) do
-    parse(rest, state, tokens)
-  end
-
-  defp string_continue("\"" <> rest, acc) do
-    # {IO.iodata_to_binary(acc), rest}
-    {acc, rest}
-  end
-
-  # defp string_continue("", _), do: syntax_error(nil, pos)
-
-  defp string_continue(string) do
-    count = string_chunk_size(string, 0)
-    <<chunk::binary-size(count), rest::binary>> = string
-    string_continue(rest, chunk)
-  end
-
-  defp string_chunk_size("\"" <> _, acc), do: acc
-  defp string_chunk_size("\\" <> _, acc), do: acc
-
-  # # Control Characters (http://seriot.ch/parsing_json.php#25)
-  # defp string_chunk_size(<<char>> <> _rest, _acc) when char <= 0x1F do
-  #   syntax_error(<<char>>)
-  # end
-
-  defp string_chunk_size(<<char>> <> rest, acc) when char < 0x80 do
-    string_chunk_size(rest, acc + 1)
-  end
-
-  defp string_chunk_size(<<codepoint::utf8>> <> rest, acc) do
-    string_chunk_size(rest, acc + string_codepoint_size(codepoint))
-  end
-
-  defp string_codepoint_size(codepoint) when codepoint < 0x800, do: 2
-  defp string_codepoint_size(codepoint) when codepoint < 0x10000, do: 3
-  defp string_codepoint_size(_), do: 4
-
-  # defp string_chunk_size(other, _acc), do: syntax_error(other, pos)
-
 end
 
---
+++ lib/ex_json_coloring/lexir.ex
@@ -0,0 +1,88 @@
+defmodule ExJsonColoring.Lexir do
+  alias ExJsonColoring.Token
+
+  @doc """
+  ## Examples
+
+    iex>ExJsonColoring.lexir ~s({"key": "value"})
+    [
+      %ExJsonColoring.Token{token: :brace, value: "{"},
+      %ExJsonColoring.Token{token: :key, value: "\\"key\\""},
+      %ExJsonColoring.Token{token: :colon, value: ":"},
+      %ExJsonColoring.Token{token: :string, value: "\\"value\\""},
+      %ExJsonColoring.Token{token: :brace, value: "}"}
+    ]
+  """
+  def lexir(json) do
+    json
+    |> parse(nil, [])
+  end
+
+  def parse("{" <> rest, state, tokens) do
+    token = %Token{token: :brace, value: "{"}
+    parse(rest, :find_key, [token] ++ tokens)
+  end
+
+  def parse("\"" <> rest, state, tokens) do
+    {string, rest} = string_continue(rest)
+
+    token_type = case state do
+      :find_key -> :key
+      _ -> :string
+    end
+
+    token = %Token{token: token_type, value: "\"#{string}\""}
+    parse(rest, :string, [token] ++ tokens)
+  end
+
+  def parse(":" <> rest, state, tokens) do
+    token = %Token{token: :colon, value: ":"}
+    parse(rest, state, [token] ++ tokens)
+  end
+
+  def parse("}" <> _, _, tokens) do
+    token = %Token{token: :brace, value: "}"}
+    [token] ++ tokens
+    |> Enum.reverse
+  end
+
+  def parse(" " <> rest, state, tokens) do
+    parse(rest, state, tokens)
+  end
+
+  defp string_continue("\"" <> rest, acc) do
+    # {IO.iodata_to_binary(acc), rest}
+    {acc, rest}
+  end
+
+  # defp string_continue("", _), do: syntax_error(nil, pos)
+
+  defp string_continue(string) do
+    count = string_chunk_size(string, 0)
+    <<chunk::binary-size(count), rest::binary>> = string
+    string_continue(rest, chunk)
+  end
+
+  defp string_chunk_size("\"" <> _, acc), do: acc
+  defp string_chunk_size("\\" <> _, acc), do: acc
+
+  # # Control Characters (http://seriot.ch/parsing_json.php#25)
+  # defp string_chunk_size(<<char>> <> _rest, _acc) when char <= 0x1F do
+  #   syntax_error(<<char>>)
+  # end
+
+  defp string_chunk_size(<<char>> <> rest, acc) when char < 0x80 do
+    string_chunk_size(rest, acc + 1)
+  end
+
+  defp string_chunk_size(<<codepoint::utf8>> <> rest, acc) do
+    string_chunk_size(rest, acc + string_codepoint_size(codepoint))
+  end
+
+  defp string_codepoint_size(codepoint) when codepoint < 0x800, do: 2
+  defp string_codepoint_size(codepoint) when codepoint < 0x10000, do: 3
+  defp string_codepoint_size(_), do: 4
+
+  # defp string_chunk_size(other, _acc), do: syntax_error(other, pos)
+
+end
