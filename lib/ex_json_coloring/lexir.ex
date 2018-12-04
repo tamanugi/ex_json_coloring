513a568211caac307c0cf86a7388406bf2324502
tamanugi <tamanugi@gmail.com>
2018-12-04 23:51:00 +0900

    number

--- lib/ex_json_coloring/lexir.ex
+++ lib/ex_json_coloring/lexir.ex
@@ -19,11 +19,30 @@ defmodule ExJsonColoring.Lexir do
     iex>ExJsonColoring.Lexir.element "null", []
     {"", [%ExJsonColoring.Token{token: :null, value: "null"}]}
 
+    iex>ExJsonColoring.Lexir.element "123", []
+    {"", [%ExJsonColoring.Token{token: :number, value: "123"}]}
+
   """
   def element(arg, acc) do
     skip_ws(arg)
     |> value(acc)
   end
+
+  # 
+  def value("", acc), do: {"", acc}
+  def value(ws, acc) when ws in '\s\n\t\r', do: {"", acc}
+
+  # number
+  def value(<<char>> <> rest, acc) when char in '123456789'  do
+    {rest_, number_val} = number(rest, [char])
+    acc = acc ++ [%Token{token: :number, value: number_val |> List.to_string}]
+    value(rest_, acc)
+  end
+
+  def number(<<char>> <> rest, acc) when char in '0123456789' do
+    number(rest, acc ++ [char])
+  end
+  def number(rest, acc), do: {rest, acc}
 
   # boolean
 
