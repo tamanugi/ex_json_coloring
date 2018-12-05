5bc058414ff158fd06cca6bd40f1bbe5c65bf247
tamanugi <tamanugi@gmail.com>
2018-12-05 22:01:32 +0900

    object1

--- lib/ex_json_coloring/lexir.ex
+++ lib/ex_json_coloring/lexir.ex
@@ -28,6 +28,17 @@ defmodule ExJsonColoring.Lexir do
     iex>ExJsonColoring.Lexir.element ~s("123456"), []
     {"", [%ExJsonColoring.Token{token: :string, value: "123456"}]}
 
+    iex>ExJsonColoring.Lexir.element ~s({"key": "value"}), []
+    {
+      "",
+      [
+        %ExJsonColoring.Token{token: :brace, value: "{"},
+        %ExJsonColoring.Token{token: :key_string, value: "key"},
+        %ExJsonColoring.Token{token: :colon, value: ":"},
+        %ExJsonColoring.Token{token: :string, value: "value"},
+        %ExJsonColoring.Token{token: :brace, value: "}"},
+      ]
+    }
 
   """
   def element(arg, acc) do
@@ -35,14 +46,37 @@ defmodule ExJsonColoring.Lexir do
     |> value(acc)
   end
 
+  # object
+  def value("{" <> rest, acc) do
+    acc = acc ++ [%Token{token: :brace, value: "{"}]
+    {rest_, str_val} = string_start(rest)
+    acc = acc ++ [%Token{token: :key_string, value: str_val}]
+
+    value(rest_, acc)
+  end
+
+  def value(":" <> rest, acc) do
+    acc = acc ++ [%Token{token: :colon, value: ":"}]
+    value(rest, acc)
+  end
+
+  def value("}" <> rest, acc) do
+    acc = acc ++ [%Token{token: :brace, value: "}"}]
+    value(rest, acc)
+  end
+
   # string
   def value("", acc), do: {"", acc}
   def value(ws, acc) when ws in '\s\n\t\r', do: {"", acc}
 
-  def value("\"" <> rest, acc) do
-    {rest_, str_val} = string(rest, "")
+  def value("\"" <> _ = string, acc) do
+    {rest, str_val} = string_start(string)
     acc = acc ++ [%Token{token: :string, value: str_val}]
-    value(rest_, acc)
+    value(rest, acc)
+  end
+
+  def string_start("\"" <> rest) do
+    string(rest, "")
   end
 
   def string("\"" <> rest, acc), do: {rest, acc}
@@ -76,6 +110,12 @@ defmodule ExJsonColoring.Lexir do
 
   def value("null" <> rest, acc) do
     {rest, acc ++ [%Token{token: :null, value: "null"}]}
+  end
+
+  # ws
+
+  def value(<<char>> <> rest, acc) when char in '\s\n\t\r' do
+    value(rest, acc)
   end
 
   def skip_ws(<<char>> <> rest) when char in  '\s\n\t\r' do
